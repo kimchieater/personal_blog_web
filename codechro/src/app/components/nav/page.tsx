d2b44fe899e13cdba8f63f9148cbec92371a612b
kimchieater <jasonj19276@gmail.com>
2024-05-15 11:43:14 +1000

    set up all the components

---
+++ codechro/src/app/components/nav/page.tsx
@@ -0,0 +1,15 @@
+
+import Link from "next/link"
+
+const Nav = () => {
+  return(
+    <div className="bg-neutral-100">
+      
+      <Link href="/"></Link>
+      <ul>
+        <Link href=""></Link>
+      </ul>
+      
+    </div>
+  )
+}
