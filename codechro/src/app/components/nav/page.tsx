63e7a05e95af69e12ee88c8acc1a1ccac3d38976
kimchieater <jasonj19276@gmail.com>
2024-05-15 18:09:50 +1000

    updated

--- codechro/src/app/components/nav/page.tsx
+++ codechro/src/app/components/nav/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 
 const Nav = () => {
   return(
+    <div>
     <div className="flex justify-between items-center leading-2 pb-5">
       <Link href="/"><span className="font-bold text-xl">Code</span><span className="font-light">Chornicles</span></Link>
       <ul className="flex gap-2 text-lg">
@@ -10,6 +11,11 @@ const Nav = () => {
         <Link href="/aboutme">About Me</Link>
         <Link href="/dashboard" className="font-bold">Admin</Link>
       </ul>
+    </div>
+    <div className="flex gap-6 justify-stretch :">
+        <div className="h-[1px] bg-neutral-950 flex-auto"></div>
+        <div className="h-[1px] bg-neutral-950 flex-auto"></div>
+      </div>
     </div>
   )
 }
