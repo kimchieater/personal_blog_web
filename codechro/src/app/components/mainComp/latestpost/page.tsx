b42b5678a2db45b276252ad0db2d81a58d725c62
kimchieater <jasonj19276@gmail.com>
2024-05-16 18:21:23 +1000

    updated

--- codechro/src/app/components/mainComp/latestpost/page.tsx
+++ codechro/src/app/components/mainComp/latestpost/page.tsx
@@ -9,11 +9,13 @@ interface LatestPostProps {
 
 const LatestPost: React.FC<LatestPostProps> = ({posts}) =>{
   return(
-    <div className="mt-[100px]">
-      {
-
-      }
-
+    <div className="mt-[100px] border-solid border-4 border-neutral-900 grid grid-cols-1 grid-rows-10">
+      <div className="border-solid border-4 border-neutral-600 p-5 bg-red-700"></div>
+      <div className="border-solid border-4 border-neutral-600 p-5 bg-red-700"></div>
+      <div className="border-solid border-4 border-neutral-600 p-5 bg-red-700"></div>
+      <div className="border-solid border-4 border-neutral-600 p-5 bg-red-700"></div>
+      <div className="border-solid border-4 border-neutral-600 p-5 bg-red-700"></div>
+      
     </div>
   )
 }
