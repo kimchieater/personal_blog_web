cea77323dd4d7ecd4c843b2f3947252772bf1113
kimchieater <jasonj19276@gmail.com>
2024-05-21 15:53:29 +1000

    Update page.tsx

--- codechro/src/app/components/footer/page.tsx
+++ codechro/src/app/components/footer/page.tsx
@@ -8,7 +8,7 @@ const Footer = () =>{
             <div className="h-[1px] bg-neutral-950 flex-auto"></div>
         </div>
         
-        <h1 className="text-center p-5">Copyrights @ Mathew 'Jason' Jeong</h1>
+        <h1 className="text-center p-5">Copyrights @ Mathew Jason Jeong</h1>
         </div>
     )
 }
